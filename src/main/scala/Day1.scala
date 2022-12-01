fd5f2d3eb53082297dff751ff26d27d87441779a
Antonio Muñoz <antonio.munoz@clarity.ai>
2022-12-01 11:18:05 +0100

    initial commit

---
+++ src/main/scala/Day1.scala
@@ -0,0 +1,14 @@
+import scala.io.Source
+
+object Day1:
+
+  def part1(lines: List[String]): Int =
+    lines.mkString(",").split(",,").map(_.split(",").map(_.toInt).sum).max
+
+  def part2(lines: List[String]): Int =
+    lines.mkString(",").split(",,").map(_.split(",").map(_.toInt).sum).sorted.reverse.take(3).sum
+
+@main def main1: Unit =
+  val input = Source.fromFile("input/day1.txt").getLines.toList
+  println(Day1.part1(input))
+  println(Day1.part2(input))
