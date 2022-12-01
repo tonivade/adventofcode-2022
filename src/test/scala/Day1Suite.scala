bc9f9b4b8140b9742731a55f1d07cc944c45e1a2
Antonio Muñoz <antonio.munoz@clarity.ai>
2022-12-01 11:50:41 +0100

    day1 unit tests refactor

--- src/test/scala/Day1Suite.scala
+++ src/test/scala/Day1Suite.scala
@@ -1,41 +1,26 @@
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class Day1Suite extends munit.FunSuite {
+  val lines = """1000
+                |2000
+                |3000
+                |
+                |4000
+                |
+                |5000
+                |6000
+                |
+                |7000
+                |8000
+                |9000
+                |
+                |10000""".stripMargin
+
   test("day 1 part 1") {
-    var lines = """1000
-                  |2000
-                  |3000
-                  |
-                  |4000
-                  |
-                  |5000
-                  |6000
-                  |
-                  |7000
-                  |8000
-                  |9000
-                  |
-                  |10000""".stripMargin
-    val result = Day1.part1(lines.split("\n").toList)
-    assertEquals(result, 24000)
+    assertEquals(Day1.part1(lines.split("\n").toList), 24000)
   }
 
   test("day 1 part 2") {
-    var lines = """1000
-                  |2000
-                  |3000
-                  |
-                  |4000
-                  |
-                  |5000
-                  |6000
-                  |
-                  |7000
-                  |8000
-                  |9000
-                  |
-                  |10000""".stripMargin
-    val result = Day1.part2(lines.split("\n").toList)
-    assertEquals(result, 45000)
+    assertEquals(Day1.part2(lines.split("\n").toList), 45000)
   }
 }
