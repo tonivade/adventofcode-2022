8cbacd735a507c5c90c903ef6b711e3a993d1c34
Antonio Muñoz <antoniogmc@gmail.com>
2022-12-08 15:34:28 +0100

    day8 part1

--- src/main/scala/Day8.scala
+++ src/main/scala/Day8.scala
@@ -8,20 +8,50 @@ object Day8:
   case class Position(x: Int, y: Int)
 
   case class Matrix(board: Map[Position, Tree]):
-    def height: Int = board.keySet.map(_._2).max
-    def width: Int = board.keySet.map(_._1).max
-    def foreach(task: (Position, Tree) => Unit): Unit =
-      board.foreachEntry(task)
+    val height: Int = board.keySet.map(_._2).max
+    val width: Int = board.keySet.map(_._1).max
+
+    def find(task: ((Position, Tree)) => Boolean): Map[Position, Tree] =
+      board.filter(task)
+
+    def visible(position: Position): Boolean = 
+      left(position).map(board(_)).forall(_ < board(position)) ||
+      right(position).map(board(_)).forall(_ < board(position)) ||
+      up(position).map(board(_)).forall(_ < board(position)) ||
+      down(position).map(board(_)).forall(_ < board(position))
+
+    def left(position: Position): List[Position] = 
+      ((position.x - 1) to 0 by -1).map(Position(_, position.y)).toList
+    def right(position: Position): List[Position] =
+      ((position.x + 1) to width).map(Position(_, position.y)).toList
+    def up(position: Position): List[Position] =
+      ((position.y - 1) to 0 by -1).map(Position(position.x, _)).toList
+    def down(position: Position): List[Position] =
+      ((position.y + 1) to height).map(Position(position.x, _)).toList
+    
+    def mkString: String =
+      (0 to height).map { y =>
+        (0 to width).map { x =>
+          board(Position(x, y))
+        }.mkString
+      }.mkString("\n")
 
   def part1(input: String): Int =
     val matrix = Matrix(input.split("\n")
       .zipWithIndex
       .flatMap {
         case (line, y) => line.zipWithIndex.map {
-          case (value, x) => (Position(x, y), x.toString().toInt)
+          case (value, x) => (Position(x, y), value.toString().toInt)
         }
       }.foldLeft(Map.empty[Position, Tree]) {
         case (map, (position, tree)) => map + (position -> tree)
       })
+    
+    matrix.find {
+        case (position, _) => matrix.visible(position)
+      }
+      .size
 
-    1
+@main def main: Unit =
+  val input = Source.fromFile("input/day8.txt").getLines().mkString("\n")
+  println(Day8.part1(input))
