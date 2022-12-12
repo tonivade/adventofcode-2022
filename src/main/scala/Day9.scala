118a13eff3215fc81599558b5fe7aa0ecc25c5a2
Antonio Muñoz <antonio.munoz@clarity.ai>
2022-12-12 12:07:08 +0100

    day9 cleanup

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -12,10 +12,8 @@ object Day9:
 
   case class Position(x: Int, y: Int):
     def overlap(other: Position): Boolean = this == other
-    def close(other: Position): Boolean = 
+    def close(other: Position): Boolean =
       Math.abs(this.x - other.x) <= 1 && Math.abs(this.y - other.y) <= 1
-    def follow(other: Position): Boolean = 
-      Math.abs(this.x - other.x) == 0 || Math.abs(this.y - other.y) == 0
     def atTop(other: Position): Boolean = other.y > this.y
     def atBottom(other: Position): Boolean = other.y < this.y
     def atLeft(other: Position): Boolean = other.x < this.x
@@ -46,27 +44,17 @@ object Day9:
       else
         this.left
 
-
   object Position:
     val zero = Position(0, 0)
 
   case class Rope(head: Position, tail: Position):
 
-    def up: Rope =
-      val newHead = head.up
-      Rope(newHead, tail moveTo newHead)
+    def moveTail(newHead: Position): Rope = Rope(newHead, tail moveTo newHead)
 
-    def down: Rope =
-      val newHead = head.down
-      Rope(newHead, tail moveTo newHead)
-
-    def left: Rope =
-      val newHead = head.left
-      Rope(newHead, tail moveTo newHead)
-
-    def right: Rope =
-      val newHead = head.right
-      Rope(newHead, tail moveTo newHead)
+    def up: Rope = moveTail(head.up)
+    def down: Rope = moveTail(head.down)
+    def left: Rope = moveTail(head.left)
+    def right: Rope = moveTail(head.right)
 
   object Rope:
     val start = Rope(Position.zero, Position.zero)
@@ -76,32 +64,20 @@ object Day9:
     val tail = knots.tail
     val last = knots.last
 
-    def up: LongRope = LongRope(tail.foldLeft(List(head.up)) {
-      case (state, current) => 
-        val next = current moveTo state.last
-        state :+ next
-    })
+    def moveTail(newHead: Position): LongRope =
+      LongRope(tail.foldLeft(List(newHead)) {
+        case (state, current) =>
+          val next = current moveTo state.last
+          state :+ next
+      })
 
-    def down: LongRope = LongRope(tail.foldLeft(List(head.down)) {
-      case (state, current) => 
-        val next = current moveTo state.last
-        state :+ next
-    })
-
-    def left: LongRope = LongRope(tail.foldLeft(List(head.left)) {
-      case (state, current) => 
-        val next = current moveTo state.last
-        state :+ next
-    })
-
-    def right: LongRope = LongRope(tail.foldLeft(List(head.right)) {
-      case (state, current) => 
-        val next = current moveTo state.last
-        state :+ next
-    })
+    def up: LongRope = moveTail(head.up)
+    def down: LongRope = moveTail(head.down)
+    def left: LongRope = moveTail(head.left)
+    def right: LongRope = moveTail(head.right)
 
   object LongRope:
-    def apply(p0: Position, p1: Position, p2: Position, p3: Position, p4: Position, 
+    def apply(p0: Position, p1: Position, p2: Position, p3: Position, p4: Position,
               p5: Position, p6: Position, p7: Position, p8: Position, p9: Position): LongRope =
       LongRope(List(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9))
     val start = LongRope(List.fill(10)(Position.zero))
@@ -134,12 +110,12 @@ object Day9:
       .toList
 
   def part1(input: String): Int =
-    val movements = parse(input) 
+    val movements = parse(input)
     val result = move(Rope.start, movements)
     result.groupBy(_.tail).size
 
   def part2(input: String): Int =
-    val movements = parse(input) 
+    val movements = parse(input)
     val result = moveLong(LongRope.start, movements)
     result.groupBy(_.last).size
 
