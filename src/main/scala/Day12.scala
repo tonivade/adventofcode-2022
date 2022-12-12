2be9ec4173d1c8cb398a4697795e255a03a07eea
Antonio Muñoz <antonio.munoz@clarity.ai>
2022-12-12 18:47:48 +0100

    Day12 part1 WIP

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -2,7 +2,7 @@ package day12
 
 import scala.io.Source
 import scala.collection.mutable.HashMap
-import java.util.PriorityQueue
+import scala.collection.mutable.ListBuffer
 
 object Day12:
 
@@ -13,11 +13,7 @@ object Day12:
     def left: Point = Point(x - 1, y)
     def right: Point = Point(x + 1, y)
 
-  class Node(val point: Point, var parent: Point, var distance: Long) extends Comparable[Node]:
-    override def compareTo(other: Node): Int =
-      if (this.distance > other.distance) 1
-      else if (this.distance < other.distance) -1
-      else 0
+  class Node(val point: Point, var parent: Point, var distance: Long):
     override def toString(): String = s"point=$point, parent=$parent, distance=$distance"
 
   case class Matrix(map: Map[Point, Char]):
@@ -31,53 +27,46 @@ object Day12:
     }.map(_._1).get
 
     def search: Long =
-      val queue = PriorityQueue[Node]()
-      map.keySet.map {
-        case p if (p == start) => Node(start, null, 0)
-        case p => Node(p, null, Long.MaxValue)
-      }.foreach(queue.add)
-
-      val routes = map.map {
-        case (from, _) => (from, moves(from).map {
-          to => (to, 1)
-        }.toMap)
-      }
-
+      val queue = ListBuffer.empty[Node]
+      queue.append(Node(start, null, 0))
       val index = HashMap.empty[Point, Node]
-      queue.forEach {
+      queue.foreach {
         node => index.put(node.point, node)
       }
 
       while(!queue.isEmpty) {
-        val node = queue.remove()
-        val edges = routes(node.point)
+        val current = queue.remove(0)
 
-        edges.foreach {
-          (to, cost) =>
-            val distance = node.distance + cost
-            val other = index(to)
-            if (distance < other.distance) {
-              other.distance = distance
-              other.parent = node.point
-              queue.remove(other)
-              queue.add(other)
-            }
+        val adjacent = moves(current.point)
+
+        adjacent.foreach {
+          to => if (!index.contains(to))
+            val next = Node(to, current.point, current.distance + 1)
+            index.put(to, next)
+            queue.append(next)
         }
       }
+
+      println(map.size)
+      println(index.size)
+      index.foreach(println)
 
       Map.from(index)(end).distance
 
     def moves(current: Point): List[Point] =
       val value = map(current)
-      val next = if (value == 'S') 'a'
-                 else if (value == 'z') 'E'
-                 else value + 1
-      current.adjacent
-        .map(p => (p, map.get(p)))
-        .filter(_._2.isDefined)
-        .map(t => (t._1, t._2.get))
-        .filter(t => t._2 == value || t._2 == next)
-        .map(_._1)
+      if (value == 'E')
+        Nil
+      else
+        val next = if (value == 'S') 'a'
+                   else if (value == 'z') 'E'
+                   else (value + 1).toChar
+        current.adjacent
+          .map(p => (p, map.get(p)))
+          .filter(_._2.isDefined)
+          .map(t => (t._1, t._2.get))
+          .filter(t => t._2 == value || t._2 == next)
+          .map(_._1)
 
   def part1(input: String): Long =
     val matrix = Matrix(input.split("\n").zipWithIndex.flatMap {
